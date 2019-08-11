7b876afcb49082afa106c2879f6e8a81a18c764c
andyh0913 <andyh0913@gmail.com>
2019-08-11 20:37:05 +0800

    add Loginpage.js

---
+++ src/container/Loginpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Loginpage extends React.Component {
+    render(){
+        return (
+            <div>Login Page</div>
+        )
+    }
+}
+
+export default Loginpage;
