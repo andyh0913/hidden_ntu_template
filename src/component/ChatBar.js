850973f2f9ab5147a3c51733ccc6eedb3ffefce5
andyh0913 <andyh0913@gmail.com>
2019-08-12 12:52:23 +0800

    modify ChatBar css

--- src/component/ChatBar.js
+++ src/component/ChatBar.js
@@ -30,9 +30,9 @@ class ChatBar extends React.Component {
 
     render(){
         return (
-            <div className="w-100 h3 bg-black o-80 flex flex-row justify-between items-center">
-                <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="說點什麼吧..." className="br-pill h-50 ml4 ph2 bg-gray ba b--gray ph-silver white flex-grow-1 fw2"/>
-                <button id="chatButton" className="bg-transparent h2 w2 border-none ma3 grow"
+            <div className="w-100 h3 bg-black o-80 ph4 flex flex-row justify-between items-center">
+                <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="說點什麼吧..." className="br-pill h2 ph2 bg-gray ba b--gray ph-silver white flex-grow-1 fw2"/>
+                <button id="chatButton" className="bg-transparent h2 w2 border-none ml3 grow"
                 onClick={this.handleButtonClick}></button>
             </div>
         )
--- src/component/SingleMessage.js
+++ src/component/SingleMessage.js
@@ -3,7 +3,7 @@ import '../App.css'
 
 const SingleMessage = (props) => {
     return (
-        <div className={`white normal mw5 br3 f5 pa2 ma0 tl ${props.isUser?"bg-blue self-end":"bg-gray self-start"}`}>{props.text}</div>
+        <div className={`white normal mw5 br3 f5 pa2 ma1 tl ${props.isUser?"bg-blue self-end":"bg-gray self-start"}`}>{props.text}</div>
     )
 }
 
