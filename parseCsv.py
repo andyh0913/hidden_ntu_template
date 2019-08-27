074ca77b3b9655118f50d02d8da2f9844b967472
andyh0913 <andyh0913@gmail.com>
2019-08-28 01:42:07 +0800

    modify chat time

--- parseCsv.py
+++ parseCsv.py
@@ -4,6 +4,7 @@ df = pd.read_csv('script.csv')
 
 for i, row in df.iterrows():
     df.at[i,'progress'] = i
-    df.at[i,'wait'] = 2 * df['wait'][i]
+    if df['wait'][i] != 0:
+        df.at[i,'wait'] =  (len(df['content'][i])-1)/10 + 1
 
 df.to_csv('result.csv', index=False)
--- src/component/MenuButton.js
+++ src/component/MenuButton.js
@@ -6,9 +6,9 @@ const MenuButton = (props) => {
         <div id="menubar-button"
         className="fixed flex flex-column items-center justify-center br-100 w3 h3 bg-white shadow-1 right-1 top-1 grow z-2"
         onClick={props.onClick}>
-            <div className="bg-silver w-50 h0 mv1"></div>
-            <div className="bg-silver w-50 h0 mv1"></div>
-            <div className="bg-silver w-50 h0 mv1"></div>
+            <div className="bg-white w-50 h0 mv1"></div>
+            <div className="bg-white w-50 h0 mv1"></div>
+            <div className="bg-white w-50 h0 mv1"></div>
         </div>
     )
 }
