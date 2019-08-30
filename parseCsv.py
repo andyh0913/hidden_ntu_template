139b4123246b897c70b234231913e6fa71097529
andyh0913 <andyh0913@gmail.com>
2019-08-31 03:20:45 +0800

    fix auto continue process

--- parseCsv.py
+++ parseCsv.py
@@ -5,6 +5,9 @@ df = pd.read_csv('script.csv')
 for i, row in df.iterrows():
     df.at[i,'progress'] = i
     if df['wait'][i] != 0:
-        df.at[i,'wait'] =  (len(df['content'][i])-1)/10 + 1
+        if df['content'][i] == "玩家訊息" or df['content'][i] == "嗶卡":
+            df.at[i,'wait'] = -1
+        else:
+            df.at[i,'wait'] =  (len(df['content'][i])-1)/10 + 1
 
 df.to_csv('result.csv', index=False)
